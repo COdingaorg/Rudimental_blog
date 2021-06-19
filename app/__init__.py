b200c5a92a7d66c123792f022fad550231f90bd8
Caleb Odinga <calemasanga@gmail.com>
2021-06-19 23:23:55 +0300

    adds csrf, flask login and flask mal and initializes them in init.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,17 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from config import config_options
 from flask_uploads import configure_uploads, UploadSet, IMAGES
+from flask_login import LoginManager
+from flask_wtf import CsrfProtect
+from flask_mail import Mail
 
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 photos = UploadSet('photos', IMAGES)
+login_manager = LoginManager()
+csrf = CsrfProtect()
+mail = Mail()
 
 
 def create_app(config_name):
@@ -20,6 +26,10 @@ def create_app(config_name):
   #inotializing extensions
   bootstrap.init_app(app)
   db.init_app(app)
+  login_manager.init_app(app)
+  csrf.init_app(app)
+  mail.init_app(app)
+
 
   #Registering blueprints
   from .main import main as main_blueprint
