925002a5c39118a7389522d1763ebfe6ac815e27
Caleb Odinga <calemasanga@gmail.com>
2021-06-20 01:28:41 +0300

    adds request and api consumption, gets reaponse and uses it create Quote instance

---
+++ app/main/requests.py
@@ -0,0 +1,27 @@
+import os
+import urllib.request, json
+from models import Quote
+
+#getting api key
+quote_url = os.environ.get('API_URL')
+
+#get quotes from api
+def get_quotes():
+  '''
+  function that returns quotes- randomly
+  '''
+  with urllib.request.urlopen(quote_url) as url:
+    quotes_response = url.read()
+    quotes_response_readable = json.loads(quotes_response)
+    quotes_item = process_results(quotes_response_readable)
+  return quotes_item
+    
+def process_results(quotes_response_readable):
+  id = quotes_response_readable.get('id')
+  author = quotes_response_readable.get('author')
+  quote = quotes_response_readable.get('quote')
+  link = quotes_response_readable.get('permalink')
+
+  new_quote = Quote(id, author, quote, link)
+
+  return new_quote
--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 export SECRET_KEY = 'rudimental'
 export MAIL_USERNAME = 'caleb.odinga@student.moringaschool.com'
 export MAIL_PASSWORD = '1lov3h3r@MS'
+export API_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
