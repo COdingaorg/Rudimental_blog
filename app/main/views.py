fac88b24ed988ec86d08565a10d6e017c1b43386
Caleb Odinga <calemasanga@gmail.com>
2021-06-22 07:51:36 +0300

    adds add blog form, add blogs photos form,and added data in categories table

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,14 @@
+from os import abort
+import re
+from flask.globals import request
+from flask_login import login_required
 from . import main
 from flask import render_template
 from .requests import get_quotes
+from flask import abort, flash, redirect, url_for
+from .. import photos, db
+from ..models import Blog, Category
+
 
 @main.route('/')
 def index():
@@ -8,3 +16,35 @@ def index():
   new_quote = get_quotes()
   return render_template('index.html', new_quote = new_quote)
 
+@main.route('/<userLogged>/addblog', methods=["GET",'POST'])
+@login_required
+def addblog():
+  if request.method == 'POST':
+    title = request.form.get('title')
+    cat = request.form.get('category')
+    category = Category.query.filter_by(category = cat).first()
+    category_id = category.category_id
+    content = request.form.get('content')
+
+    blog = Blog(title = title, category = category_id, content = content)
+    db.session.add(blog)
+    db.session.commit()
+
+    return redirect(url_for('main.addblogphotos',blogn = blog.blog_id, blog=blog))
+  return render_template('blog.html')
+
+
+
+@main.route('/upload/<blogn>', methods=['GET','POST'])
+@login_required
+def addblogphotos(blogn):
+  blog = Blog.query.filter_by(blog_id = blogn).first()
+  if request.method =='POST' and 'photo' in request.files:
+    filename = photos.save(request.files['photo'])
+    path = f'photos/{filename}'
+    blog.photo_path = path
+    db.session.commit()
+    
+    flash('photo saved')
+
+  return render_template('addblogphotos.html', blog = blog)
--- app/models.py
+++ app/models.py
@@ -23,6 +23,7 @@ def load_user(user_id):
 class Blog(db.Model):
   __tablename__='blogs'
   blog_id = db.Column(db.Integer, primary_key = True)
+  photo_path = db.Column(db.String())
   title = db.Column(db.String(255))
   content = db.Column(db.String(255))
   datePosted = db.Column(db.DateTime, default=datetime.utcnow)
